f63efcdd974d1f120332feeb721cc67c39905457
lars <lmaischein@gmail.com>
2020-08-02 21:02:03 +0200

    updated minicards

--- src/app/mini-card/mini-card-data.ts
+++ src/app/mini-card/mini-card-data.ts
@@ -1,11 +1,11 @@
 // tslint:disable-next-line: no-unused-expression
 export const MINICARDDATA = [
   // tslint:disable-next-line: max-line-length
-  { icon: 'payments', title: 'Gesamt Umsatz', value: 5432, color: 'primary', isIncrease: true, isCurrency: true, duration: 'seit letztem Monat', percentValue: 53.21 },
+  { icon: 'payments', title: 'Gesamt Umsatz', value: 5432, color: 'primary', isIncrease: true, isCurrency: true, duration: 'seit letztem Monat', percentValue: .5 },
   // tslint:disable-next-line: max-line-length
-  { icon: 'euro_symbol', title: 'Durchschnittlicher Umsatz pro Bestellung', value: 128, color: 'accent', isIncrease: false, isCurrency: true, duration: 'seit letztem Monat', percentValue: 28.45 },
+  { icon: 'euro_symbol', title: 'Durchschnittlicher Umsatz', value: 128, color: 'accent', isIncrease: false, isCurrency: true, duration: 'seit letztem Monat', percentValue: .28 },
   // tslint:disable-next-line: max-line-length
-  { icon: 'shopping_cart', title: 'Gesamt Bestellungen', value: 56, color: 'warn', isIncrease: true, isCurrency: false, duration: 'seit letztem Monat', percentValue: 35.11 },
+  { icon: 'shopping_cart', title: 'Gesamt Bestellungen', value: 56, color: 'warn', isIncrease: true, isCurrency: false, duration: 'seit letztem Monat', percentValue: .35 },
   // tslint:disable-next-line: max-line-length
-  { icon: 'account_box', title: 'Neue Kunden', value: 23, color: 'primary', isIncrease: false, isCurrency: false, duration: 'seit letztem Monat', percentValue: 9.51 }
+  { icon: 'account_box', title: 'Neue Kunden', value: 23, color: 'primary', isIncrease: false, isCurrency: false, duration: 'seit letztem Monat', percentValue: .09 }
 ];
