28ffc9c3eb5f42f59c9962a98f220aabf5f1bfec
lars <lmaischein@gmail.com>
2020-08-02 18:29:26 +0200

    updated charts

--- src/app/charts/annual-sales-chart/annual-sales-chart.component.ts
+++ src/app/charts/annual-sales-chart/annual-sales-chart.component.ts
@@ -10,17 +10,18 @@ import { Color, Label } from 'ng2-charts';
 export class AnnualSalesChartComponent implements OnInit {
 
   public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [65, 59, 80, 81, 56, 55, 40], label: '3 Monats Abo' },
+    { data: [56, 25, 85, 25, 64, 69, 48], label: '2 Monats Abo' },
   ];
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: ChartOptions = {
     responsive: true,
   };
   public lineChartColors: Color[] = [
-    {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)',
-    },
+    // {
+    //   borderColor: 'black',
+    //   backgroundColor: 'rgba(255,0,0,0.3)',
+    // },
   ];
   public lineChartLegend = true;
   public lineChartType: ChartType = 'line';
